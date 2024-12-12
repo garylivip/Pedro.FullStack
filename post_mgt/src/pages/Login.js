a53990b192de2475b786aed47dd48a16d853036e
Gary Li <garylivip@outlook.com>
2024-12-12 22:22:51 +0800

    Course [8] register&login completed

--- post_mgt/src/pages/Login.js
+++ post_mgt/src/pages/Login.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(event.target.username.value, event.target.password.value);
+    const data = { username: username, password: password };
+    console.log(data);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/users/login",
+        data
+      );
+      console.log("Login successful:", response.data);
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
   return (
-    <div>
-      Login
+    <div className="loginContainer">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
